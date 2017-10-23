a4945ed69ac42a5f5b39a3e912bddbd13a223a92
Katie Maxwell <katie@codeinstitute.net>
2017-10-23 16:00:02 +0000

    simple search

--- twitweb.py
+++ twitweb.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 import os
 
 app = Flask(__name__)
 
+previous_searches = set()
+
+fake_news = ["fakenews1", "fakenews2", "fakenews3"]
+
 @app.route("/")
 def index():
-    return render_template("banana.html")
+    return render_template("search.html")
 
-@app.route("/photos/car/<id>")
-def get_photos_of_car(id):
-    return "You picked photos of car {0}".format(id)
+@app.route("/search")
+def do_search():
+    q = request.args.get('query')
+    previous_searches.add(q)
+    return render_template("results.html", searched_for=q, headlines=fake_news)
+
+@app.route("/previous")
+def show_prev():
+    return render_template("previous.html", search_terms = previous_searches)
+
+
+# @app.route("/photos/car/<id>")
+# def get_photos_of_car(id):
+#     return "You picked photos of car {0}".format(id)
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
